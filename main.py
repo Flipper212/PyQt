343d27daeee0a076205cbcebd747541057a51e3c
Stas <64963922+Flipper212@users.noreply.github.com>
2023-02-10 12:49:41 +0200

    Work calculator without Math

--- main.py
+++ main.py
@@ -1,42 +1,284 @@
-from PyQt5 import QtWidgets
-from PyQt5.QtWidgets import QApplication, QMainWindow
-import sys
+from PyQt5 import QtCore, QtGui, QtWidgets
+from sys import argv, exit
 
 
-class Window(QMainWindow):
-    def __init__(self):
-        super(Window, self).__init__() # 1 - цей клас 2 - self
-        self.setWindowTitle("Program PyQt")
-        self.setGeometry(300, 250, 800, 500)  # 1,2 - віддалення(x, y)    3,4 - розмір
 
-        self.text = QtWidgets.QLabel(self)  # До якого вікна відноситься (window)
-        '''
-        QLabel - текст
-        QPlainTextEdit - поле вводу
-        QPushButton - кнопка
-        '''
-        self.text.setText("Hello World")  # задати текст
-        self.text.move(100, 100)
-        self.text.adjustSize()  # фіксить відстань
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.setFixedSize(281, 341)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
 
-        self.button = QtWidgets.QPushButton(self)
-        self.button.move(70, 150)
-        self.button.setText("Tap")
-        self.button.setFixedWidth(200)  # фіксований розмір
-        self.button.clicked.connect(self.tap_button)
+        self.operator_function = False # Чи виклик оператора
+        self.index = None # Знак
+        self.first_number = None # Перше число
+        self.second_number = None # Друге число
 
-    def tap_button(self):
-        self.setStyleSheet('background-color: grey;')
-        self.text.setText("Hello All")
-        self.text.adjustSize()
+        self.label_result = QtWidgets.QLabel(self.centralwidget)
+        self.label_result.setGeometry(QtCore.QRect(0, 0, 281, 60))
+        font = QtGui.QFont()
+        font.setPointSize(40)
+        font.setBold(True)
+        font.setItalic(False)
+        font.setUnderline(False)
+        font.setWeight(75)
+        font.setStrikeOut(False)
+        font.setKerning(True)
+        self.label_result.setFont(font)
+        self.label_result.setStyleSheet("background-color: rgb(211, 211, 211);")
+        self.label_result.setObjectName("label_result")
+        self.button_1 = QtWidgets.QPushButton(self.centralwidget)
+        self.button_1.setGeometry(QtCore.QRect(0, 60, 70, 70))
+        font = QtGui.QFont()
+        font.setFamily("Arial")
+        font.setPointSize(40)
+        font.setBold(True)
+        font.setItalic(False)
+        font.setWeight(75)
+        self.button_1.setFont(font)
+        self.button_1.setObjectName("button_1")
+        self.button_3 = QtWidgets.QPushButton(self.centralwidget)
+        self.button_3.setGeometry(QtCore.QRect(140, 60, 70, 70))
+        font = QtGui.QFont()
+        font.setFamily("Arial")
+        font.setPointSize(40)
+        font.setBold(True)
+        font.setItalic(False)
+        font.setWeight(75)
+        self.button_3.setFont(font)
+        self.button_3.setObjectName("button_3")
+        self.button_2 = QtWidgets.QPushButton(self.centralwidget)
+        self.button_2.setGeometry(QtCore.QRect(70, 60, 70, 70))
+        font = QtGui.QFont()
+        font.setFamily("Arial")
+        font.setPointSize(40)
+        font.setBold(True)
+        font.setItalic(False)
+        font.setWeight(75)
+        self.button_2.setFont(font)
+        self.button_2.setObjectName("button_2")
+        self.button_6 = QtWidgets.QPushButton(self.centralwidget)
+        self.button_6.setGeometry(QtCore.QRect(140, 130, 70, 70))
+        font = QtGui.QFont()
+        font.setFamily("Arial")
+        font.setPointSize(40)
+        font.setBold(True)
+        font.setItalic(False)
+        font.setWeight(75)
+        self.button_6.setFont(font)
+        self.button_6.setObjectName("button_6")
+        self.button_4 = QtWidgets.QPushButton(self.centralwidget)
+        self.button_4.setGeometry(QtCore.QRect(0, 130, 70, 70))
+        font = QtGui.QFont()
+        font.setFamily("Arial")
+        font.setPointSize(40)
+        font.setBold(True)
+        font.setItalic(False)
+        font.setWeight(75)
+        self.button_4.setFont(font)
+        self.button_4.setObjectName("button_4")
+        self.button_5 = QtWidgets.QPushButton(self.centralwidget)
+        self.button_5.setGeometry(QtCore.QRect(70, 130, 70, 70))
+        font = QtGui.QFont()
+        font.setFamily("Arial")
+        font.setPointSize(40)
+        font.setBold(True)
+        font.setItalic(False)
+        font.setWeight(75)
+        self.button_5.setFont(font)
+        self.button_5.setObjectName("button_5")
+        self.button_9 = QtWidgets.QPushButton(self.centralwidget)
+        self.button_9.setGeometry(QtCore.QRect(140, 200, 70, 70))
+        font = QtGui.QFont()
+        font.setFamily("Arial")
+        font.setPointSize(40)
+        font.setBold(True)
+        font.setItalic(False)
+        font.setWeight(75)
+        self.button_9.setFont(font)
+        self.button_9.setObjectName("button_9")
+        self.button_7 = QtWidgets.QPushButton(self.centralwidget)
+        self.button_7.setGeometry(QtCore.QRect(0, 200, 70, 70))
+        font = QtGui.QFont()
+        font.setFamily("Arial")
+        font.setPointSize(40)
+        font.setBold(True)
+        font.setItalic(False)
+        font.setWeight(75)
+        self.button_7.setFont(font)
+        self.button_7.setObjectName("button_7")
+        self.button_8 = QtWidgets.QPushButton(self.centralwidget)
+        self.button_8.setGeometry(QtCore.QRect(70, 200, 70, 70))
+        font = QtGui.QFont()
+        font.setFamily("Arial")
+        font.setPointSize(40)
+        font.setBold(True)
+        font.setItalic(False)
+        font.setWeight(75)
+        self.button_8.setFont(font)
+        self.button_8.setObjectName("button_8")
+        self.button_0 = QtWidgets.QPushButton(self.centralwidget)
+        self.button_0.setGeometry(QtCore.QRect(0, 270, 70, 70))
+        font = QtGui.QFont()
+        font.setFamily("Arial")
+        font.setPointSize(40)
+        font.setBold(True)
+        font.setItalic(False)
+        font.setWeight(75)
+        self.button_0.setFont(font)
+        self.button_0.setObjectName("button_0")
+        self.button_equal = QtWidgets.QPushButton(self.centralwidget)
+        self.button_equal.setGeometry(QtCore.QRect(70, 270, 70, 70))
+        font = QtGui.QFont()
+        font.setFamily("Arial")
+        font.setPointSize(40)
+        font.setBold(True)
+        font.setItalic(False)
+        font.setWeight(75)
+        self.button_equal.setFont(font)
+        self.button_equal.setObjectName("button_equal")
+        self.button_plus = QtWidgets.QPushButton(self.centralwidget)
+        self.button_plus.setGeometry(QtCore.QRect(210, 60, 70, 70))
+        font = QtGui.QFont()
+        font.setFamily("Arial")
+        font.setPointSize(40)
+        font.setBold(True)
+        font.setItalic(False)
+        font.setWeight(75)
+        self.button_plus.setFont(font)
+        self.button_plus.setStyleSheet("")
+        self.button_plus.setObjectName("button_plus")
+        self.button_division = QtWidgets.QPushButton(self.centralwidget)
+        self.button_division.setGeometry(QtCore.QRect(210, 270, 70, 70))
+        font = QtGui.QFont()
+        font.setFamily("Arial")
+        font.setPointSize(40)
+        font.setBold(True)
+        font.setItalic(False)
+        font.setWeight(75)
+        self.button_division.setFont(font)
+        self.button_division.setObjectName("button_division")
+        self.button_multiplication = QtWidgets.QPushButton(self.centralwidget)
+        self.button_multiplication.setGeometry(QtCore.QRect(210, 200, 70, 70))
+        font = QtGui.QFont()
+        font.setFamily("Arial")
+        font.setPointSize(40)
+        font.setBold(True)
+        font.setItalic(False)
+        font.setWeight(75)
+        self.button_multiplication.setFont(font)
+        self.button_multiplication.setObjectName("button_multiplication")
+        self.button_minus = QtWidgets.QPushButton(self.centralwidget)
+        self.button_minus.setGeometry(QtCore.QRect(210, 130, 70, 70))
+        font = QtGui.QFont()
+        font.setFamily("Arial")
+        font.setPointSize(40)
+        font.setBold(True)
+        font.setItalic(False)
+        font.setWeight(75)
+        self.button_minus.setFont(font)
+        self.button_minus.setObjectName("button_minus")
+        self.button_erase = QtWidgets.QPushButton(self.centralwidget)
+        self.button_erase.setGeometry(QtCore.QRect(140, 270, 70, 70))
+        font = QtGui.QFont()
+        font.setFamily("Arial")
+        font.setPointSize(40)
+        font.setBold(True)
+        font.setItalic(False)
+        font.setWeight(75)
+        self.button_erase.setFont(font)
+        self.button_erase.setObjectName("button_erase")
+        MainWindow.setCentralWidget(self.centralwidget)
 
-def application():
-    app = QApplication(sys.argv)
-    window = Window()
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
-    window.show()
-    sys.exit(app.exec())
+        self.add_function()
+
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "Calculator"))
+        self.label_result.setText(_translate("MainWindow", "0"))
+        self.button_1.setText(_translate("MainWindow", "1"))
+        self.button_3.setText(_translate("MainWindow", "3"))
+        self.button_2.setText(_translate("MainWindow", "2"))
+        self.button_6.setText(_translate("MainWindow", "6"))
+        self.button_4.setText(_translate("MainWindow", "4"))
+        self.button_5.setText(_translate("MainWindow", "5"))
+        self.button_9.setText(_translate("MainWindow", "9"))
+        self.button_7.setText(_translate("MainWindow", "7"))
+        self.button_8.setText(_translate("MainWindow", "8"))
+        self.button_0.setText(_translate("MainWindow", "0"))
+        self.button_equal.setText(_translate("MainWindow", "="))
+        self.button_plus.setText(_translate("MainWindow", "+"))
+        self.button_division.setText(_translate("MainWindow", "/"))
+        self.button_multiplication.setText(_translate("MainWindow", "x"))
+        self.button_minus.setText(_translate("MainWindow", "-"))
+        self.button_erase.setText(_translate("MainWindow", "⇦"))
+
+    def add_function(self):
+        self.button_0.clicked.connect(lambda: self.write_number(self.button_0.text()))
+        self.button_1.clicked.connect(lambda: self.write_number(self.button_1.text()))
+        self.button_2.clicked.connect(lambda: self.write_number(self.button_2.text()))
+        self.button_3.clicked.connect(lambda: self.write_number(self.button_3.text()))
+        self.button_4.clicked.connect(lambda: self.write_number(self.button_4.text()))
+        self.button_5.clicked.connect(lambda: self.write_number(self.button_5.text()))
+        self.button_6.clicked.connect(lambda: self.write_number(self.button_6.text()))
+        self.button_7.clicked.connect(lambda: self.write_number(self.button_7.text()))
+        self.button_8.clicked.connect(lambda: self.write_number(self.button_8.text()))
+        self.button_9.clicked.connect(lambda: self.write_number(self.button_9.text()))
+        self.button_erase.clicked.connect(lambda: self.erase_number())
+        self.button_plus.clicked.connect(lambda: self.operators("+"))
+        self.button_minus.clicked.connect(lambda: self.operators("-"))
+        self.button_multiplication.clicked.connect(lambda: self.operators("x"))
+        self.button_division.clicked.connect(lambda: self.operators("/"))
+        self.button_equal.clicked.connect(lambda: self.equal(self.label_result.text()))
+
+    def operators(self, op): # Функція при виклику опепратора (+, -, *, /)
+        self.first_number = int(self.label_result.text())
+        self.index = op
+        self.operator_function = True
+
+    def equal(self, second_number): # Функція при виклику дорівнює
+        self.second_number = int(self.label_result.text())
+        self.counter()
+
+    def write_number(self, number): # Написати число
+        if self.operator_function == True:
+            self.label_result.setText(number)
+            self.operator_function = False
+        elif self.label_result.text() == "0":
+            self.label_result.setText(number)
+        else: self.label_result.setText(self.label_result.text() + number)
+
+    def erase_number(self): # Видалити чтсло
+        if len(self.label_result.text()) == 0 or self.label_result.text() == "Error":
+            self.label_result.setText("0")
+        if self.label_result.text() != "0":
+            self.label_result.setText(self.label_result.text()[:-1])
+
+
+    def counter(self): # Загальний підрахунок, вивід результату
+        try:
+            if self.index == "+": self.label_result.setText(str(self.first_number + self.second_number))
+            elif self.index == "-": self.label_result.setText(str(self.first_number - self.second_number))
+            elif self.index == "x": self.label_result.setText(str(self.first_number * self.second_number))
+            elif self.index == "/": self.label_result.setText(str(self.first_number / self.second_number))
+        except Exception:
+            self.label_result.setText("Error")
+        finally:
+            self.operator_function = False
+            self.index = None
+            self.first_number = None
+            self.second_number = None
 
 
 if __name__ == "__main__":
-    application()
+    app = QtWidgets.QApplication(argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    exit(app.exec_())
